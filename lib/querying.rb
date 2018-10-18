eb89aa9fd9ae8975076fde5e92ba4b9f8a07a262
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-18 19:10:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.id) AS num_books_in
+  "SELECT characters.name, COUNT(character_books.character_id) AS num_books_in
   FROM characters INNER JOIN character_books ON characters.id = character_books.character_id
   INNER JOIN books ON books.id = character_books.book_id
   "
